dfb20e90851260a304c6621381687c7c248fa5d5
Ayush Pawar <ayushpawar27@gmail.com>
2023-06-24 09:39:16 +0530

    storing data in db

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
+// const { query, validationResult } = require('express-validator');
 
 // create a User Using: POST "/api/auth". Doesn't require Auth
 router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
-})
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
+}
+// [
+    // query('name'),
+    // query('email'),
+    // query('password'),
+// ],(req,res)=>{
+  // res.send({`Error Invalid, ${req.query.email}`});
+// }
+)
 
-module.exports =router
+module.exports = router
